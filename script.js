2dbf8af58e858224dc15f1c70272781e8a665741
Edu Moreno <e.m.morenolp@gmail.com>
2024-04-06 23:05:24 -0300

    estilos mod

--- script.js
+++ script.js
@@ -129,19 +129,20 @@ console.log("Bienvenido a PetShop Web")
 
 
 
- // MisterEgg
- var secuenciaDeseada = ['ArrowUp', 'ArrowRight', 'ArrowRight', 'ArrowUp', 'ArrowLeft'];
- var teclasPresionadas = [];
- console.log("Para ver los integrantes Truquito de PC : '↑', '→', '→', '↑', '←' ")
- function verificarSecuencia() {
-     if (teclasPresionadas.length === secuenciaDeseada.length) {
-         if (teclasPresionadas.every((tecla, indice) => tecla === secuenciaDeseada[indice])) {
-             window.location.href = '/integrantes.html';
-         }
-         teclasPresionadas = [];
-     }
- }
- document.addEventListener('keydown', function (event) {
-     teclasPresionadas.push(event.key);
-     verificarSecuencia();
- });
+// MisterEgg
+var secuenciaDeseada = ['ArrowUp', 'ArrowRight', 'ArrowRight', 'ArrowUp', 'ArrowLeft'];
+var teclasPresionadas = [];
+console.log("Para ver los integrantes Truquito de PC : '↑', '→', '→', '↑', '←' ")
+function verificarSecuencia() {
+    console.log(teclasPresionadas);
+    if (teclasPresionadas.length === secuenciaDeseada.length) {
+        if (teclasPresionadas.every((tecla, indice) => tecla === secuenciaDeseada[indice])) {
+            window.location.href = 'integrantes.html';
+        }
+        teclasPresionadas = [];
+    }
+}
+document.addEventListener('keydown', function (event) {
+    teclasPresionadas.push(event.key);
+    verificarSecuencia();
+});
