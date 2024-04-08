157222e8fa622e0061533f3500592bb73bc44b97
Edu Moreno <e.m.morenolp@gmail.com>
2024-04-08 00:23:27 -0300

    add Carrito

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 console.log("Bienvenido a PetShop Web")
 
 
+var botones = document.querySelectorAll(".addCarrito");
 
+botones.forEach(function (boton) {
+    boton.addEventListener("click", function () {
+        console.log("Boton presionado");
+        var contador = document.querySelector(".contador");
+        var valorActual = parseInt(contador.innerText);
+        valorActual++;
+        contador.innerText = valorActual;
+    })
+})
 
 
 
@@ -128,21 +138,20 @@ console.log("Bienvenido a PetShop Web")
 
 
 
-
-// MisterEgg
-var secuenciaDeseada = ['ArrowUp', 'ArrowRight', 'ArrowRight', 'ArrowUp', 'ArrowLeft'];
-var teclasPresionadas = [];
-console.log("Para ver los integrantes Truquito de PC : '↑', '→', '→', '↑', '←' ")
-function verificarSecuencia() {
-    console.log(teclasPresionadas);
-    if (teclasPresionadas.length === secuenciaDeseada.length) {
-        if (teclasPresionadas.every((tecla, indice) => tecla === secuenciaDeseada[indice])) {
-            window.location.href = 'integrantes.html';
+    // MisterEgg
+    var secuenciaDeseada = ['ArrowUp', 'ArrowRight', 'ArrowRight', 'ArrowUp', 'ArrowLeft'];
+    var teclasPresionadas = [];
+    console.log("Para ver los integrantes Truquito de PC : '↑', '→', '→', '↑', '←' ")
+    function verificarSecuencia() {
+        console.log(teclasPresionadas);
+        if (teclasPresionadas.length === secuenciaDeseada.length) {
+            if (teclasPresionadas.every((tecla, indice) => tecla === secuenciaDeseada[indice])) {
+                window.location.href = 'integrantes.html';
+            }
+            teclasPresionadas = [];
         }
-        teclasPresionadas = [];
     }
-}
-document.addEventListener('keydown', function (event) {
-    teclasPresionadas.push(event.key);
-    verificarSecuencia();
-});
+    document.addEventListener('keydown', function (event) {
+        teclasPresionadas.push(event.key);
+        verificarSecuencia();
+    });
